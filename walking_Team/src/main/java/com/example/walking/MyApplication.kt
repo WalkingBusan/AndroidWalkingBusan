e416bb81ee34b18537adae2e4a541f3162a95d52
ha01032 <ha01032@naver.com>
2023-03-24 16:39:52 +0900

    230324 TeamProject 회원가입, 로그인 구현 확인

---
+++ walking_Team/src/main/java/com/example/walking/MyApplication.kt
@@ -0,0 +1,20 @@
+package com.example.walking
+
+import android.app.Application
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MyApplication: Application() {
+
+    var networkService: INetworkService
+    val retrofit: Retrofit
+        get() = Retrofit.Builder()
+            // .baseUrl("http://192.168.0.21:8083/")
+            .baseUrl("http://10.100.104.205:8083/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    init {
+        networkService = retrofit.create(INetworkService::class.java)
+    }
+}
